f951a3529522c48ad91996aba4c412c1434c0292
Scott Searcy <smsearcy@gmail.com>
2020-06-09 21:53:01 -0700

    Lowered required version of Python to 3.7. Added some requirements for future development.

--- pymeshmap/__init__.py
+++ pymeshmap/__init__.py
@@ -1,3 +1,8 @@
-from importlib.metadata import version
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib.metadata import version
+else:
+    from importlib_metadata import version  # type: ignore
 
 __version__ = version("pymeshmap")
