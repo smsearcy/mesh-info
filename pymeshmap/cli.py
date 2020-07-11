e7a90cbde7816edb0575130bd2227c9487d8c807
Scott Searcy <smsearcy@gmail.com>
2020-07-11 16:05:25 -0700

    Finished up the basic network reporting, including link information. Consolidated crawler and parser functionality into a single module. Based on further testing, eliminated HTTP timeout, added max connections. Updated README and cleaned up some internal documentation.

--- pymeshmap/cli.py
+++ pymeshmap/cli.py
@@ -10,6 +10,7 @@ import re
 import string
 import sys
 import time
+from pathlib import Path
 from typing import Any, Dict, List
 
 import click
@@ -17,7 +18,9 @@ from faker import Faker  # type: ignore
 from loguru import logger
 
 from pymeshmap import crawler
-from pymeshmap.parser import SystemInfo
+
+VERBOSE_TO_LOGGING = {0: "SUCCESS", 1: "INFO", 2: "DEBUG", 3: "TRACE"}
+
 
 # TODO: replace these with proper configuration
 LOCAL_NODE_NAME = "localnode.local.mesh"
@@ -33,34 +36,78 @@ def main(ctx):
     return
 
 
-@main.command(help="Prints information about the nodes and links in the network")
+@main.command()
 @click.argument("hostname", default="localnode.local.mesh")
-@click.option("-v", "--verbose", count=True)
-@click.option("--save-errors", is_flag=True)
-def network_report(hostname: str, verbose: int, save_errors: bool):
-    """Prints information about the nodes and links in the network.
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    help="Increase logging output by specifying -v up to -vvv",
+)
+@click.option(
+    "--save-errors",
+    is_flag=True,
+    help="Saves node responses that caused errors to the current directory",
+)
+@click.option(
+    "--path",
+    type=click.Path(file_okay=False, exists=True),
+    default=".",
+    help="Path to save files to",
+)
+def network_report(hostname: str, verbose: int, save_errors: bool, path: str):
+    """Crawls network and prints information about the nodes and links.
+
+    Detailed output is not printed until the crawler finishes.
+    Increase amount of logging output by passing '--verbose'.
+
+    Does not use or require a database.
+
+    Default hostname is "localnode.local.mesh".
 
     """
 
-    log_level = {0: "WARN", 1: "INFO", 2: "DEBUG", 3: "TRACE"}.get(verbose, "WARN")
+    log_level = VERBOSE_TO_LOGGING.get(verbose, "SUCCESS")
     logger.remove()
     logger.add(sys.stderr, level=log_level)
 
     start_time = time.monotonic()
 
+    output_path = Path(path)
+
     async_debug = log_level == "DEBUG"
-    nodes = asyncio.run(
-        crawler.network_nodes(hostname, save_errors=save_errors), debug=async_debug
+    nodes, links, errors = asyncio.run(
+        crawler.network_info(hostname), debug=async_debug
     )
 
+    click.secho(f"Successfully gathered results for {len(nodes):,d} nodes", fg="blue")
     for node in nodes:
         pprint_node(node)
+
+    for link in links:
+        pprint_link(link)
+
+    if len(errors) == 0:
+        click.secho("No errors!", fg="green")
+    else:
+        click.secho(f"Encountered errors with {len(errors):,d} nodes", fg="red")
+        if save_errors:
+            click.echo("Saving responses for nodes with errors")
+        else:
+            click.echo(
+                "Use the --save-errors option to save responses from nodes with errors"
+            )
+        for ip_address, (error, response) in errors.items():
+            # TODO: MeshMap did a reverse DNS lookup to get the node name
+            click.secho(f"{ip_address}: {error!s}", fg="yellow")
+            if save_errors:
+                open(output_path / f"{ip_address}-response.txt", "w").write(response)
 
     total_time = time.monotonic() - start_time
     click.secho(f"Network report took {total_time:.2f} seconds", fg="blue")
 
 
-def pprint_node(node: SystemInfo):
+def pprint_node(node: crawler.SystemInfo):
     """Pretty print information about an AREDN node."""
     click.echo("Name: ", nl=False)
     click.secho(node.node_name, fg="blue")
@@ -70,12 +117,15 @@ def pprint_node(node: SystemInfo):
     else:
         click.secho("No MAC address found!", fg="red")
     click.echo(f"Model: {node.model}")
-    # TODO: add check for current firmware
+
+    # TODO: add check for current firmware (and "Linux?")
     click.echo(f"Firmware: {node.firmware_manufacturer} {node.firmware_version}")
     click.echo(f"LAN IP: {node.lan_ip_address}\tWAN IP: {node.wifi_ip_address}")
     click.echo("Location: ", nl=False)
     if node.latitude and node.longitude:
         click.secho(f"{node.latitude}, {node.longitude}", fg="green")
+    elif node.grid_square:
+        click.secho(node.grid_square, fg="yellow")
     else:
         click.secho("Not Available", fg="yellow")
     click.echo("Uptime: ", nl=False)
@@ -84,13 +134,22 @@ def pprint_node(node: SystemInfo):
     else:
         click.secho("Not Available", fg="yellow")
     click.echo("Loads: ", nl=False)
+
+    def colorize_load(value: float) -> str:
+        if value > 1:
+            return "red"
+        elif value > 0.5:
+            return "yellow"
+        else:
+            return "green"
+
     if node.load_averages:
-        # TODO: colorize this output
-        click.echo(
-            f"1 min: {node.load_averages[0]}; "
-            f"5 min: {node.load_averages[1]}; "
-            f"15 min: {node.load_averages[2]}"
-        )
+        load_colors = [colorize_load(load) for load in node.load_averages]
+        click.secho(str(node.load_averages[0]), nl=False, fg=load_colors[0])
+        click.echo(", ", nl=False)
+        click.secho(str(node.load_averages[1]), nl=False, fg=load_colors[1])
+        click.echo(", ", nl=False)
+        click.secho(str(node.load_averages[2]), fg=load_colors[2])
     else:
         click.secho("Not Available", fg="yellow")
     click.echo("MESH RF: ", nl=False)
@@ -102,10 +161,35 @@ def pprint_node(node: SystemInfo):
         click.secho("on", nl=False, fg="green")
         click.echo("\tSSID: ", nl=False)
         click.secho(node.ssid, fg="green")
+    click.echo(f"Channel: {node.channel}\tBand: ", nl=False)
+    # good use for walrus operator (:=) if require Python 3.8
+    # (or just make it a cached property)
+    band = node.band
+    if band != "Unknown":
+        click.echo(band)
+    else:
+        click.secho(band, fg="yellow")
     click.echo("API Version: ", nl=False)
     click.secho(node.api_version, fg=API_VERSIONS.get(node.api_version, "bright_red"))
     click.echo(f"Tunnels: {node.tunnel_installed}\tCount: {node.active_tunnel_count}")
     click.echo()
+
+
+def pprint_link(link: crawler.LinkInfo):
+    click.echo(f"{link.source} -> {link.destination} cost: ", nl=False)
+    if link.cost > 10:
+        click.secho(f"{link.cost:.3f}", fg="bright_red", bold=True)
+    elif link.cost > 6:
+        click.secho(f"{link.cost:.3f}", fg="red")
+    elif link.cost > 4:
+        click.secho(f"{link.cost:.3f}", fg="bright_yellow", bold=True)
+    elif link.cost > 2:
+        click.secho(f"{link.cost:.3f}", fg="green")
+    elif link.cost > 0.1:
+        click.secho(f"{link.cost:.3f}", fg="bright_green", bold=True)
+    else:
+        # is this a tunnel?
+        click.echo(f"{link.cost:.3f}")
 
 
 @main.command(help="Scrub identifiable information from data files for testing.")
--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,5 +1,8 @@
 """Test network crawling functionality."""
 
+import json
+from pathlib import Path
+
 import pytest
 
 from pymeshmap import crawler
@@ -12,11 +15,129 @@ async def olsr_records(filename):
             yield line.rstrip()
 
 
+@pytest.mark.parametrize(
+    "filename",
+    list(Path(__file__).parent.glob("data/sysinfo-*.json")),
+    ids=lambda obj: obj.name,
+)
+def test_parse_all_sysinfo_examples(filename):
+    """Simply validate that all sample 'sysinfo.json' files parse without errors."""
+    with open(filename, "r") as f:
+        json_data = json.load(f)
+    system_info = crawler._load_node_data(json_data)
+    assert system_info is not None
+
+    # Make sure we identified the wireless IP address
+    assert system_info.wifi_ip_address != ""
+
+
+def test_api_version_1_0(data_folder):
+    """Test parsing API version 1.0"""
+
+    with open(data_folder / "sysinfo-1.0-sample.json", "r") as f:
+        json_data = json.load(f)
+    system_info = crawler._load_node_data(json_data)
+
+    # I could just construct a second object but I'm not checking everything
+    assert system_info.node_name == "N0CALL-Oceanside-West"
+    assert len(system_info.interfaces) == 5
+    assert system_info.model == "Ubiquiti Rocket M"
+    assert system_info.grid_square == ""
+    assert system_info.latitude == -38.053394
+    assert system_info.longitude == -6.193114
+    assert system_info.ssid == "ArednMeshNetwork"
+    assert system_info.channel == "-2"
+    assert system_info.channel_bandwidth == "5"
+    assert system_info.api_version == "1.0"
+    assert not system_info.tunnel_installed
+
+
+def test_api_version_1_5(data_folder):
+    """Test parsing API version 1.5"""
+
+    with open(data_folder / "sysinfo-1.5-sample.json", "r") as f:
+        json_data = json.load(f)
+    system_info = crawler._load_node_data(json_data)
+
+    # I could just construct a second object but I'm not checking everything
+    assert system_info.node_name == "N0CALL-bm2-1"
+    assert len(system_info.interfaces) == 4
+    assert system_info.interfaces["eth0"].ip_address == "10.206.233.110"
+    assert system_info.model == "Bullet M2 HP "
+    assert system_info.grid_square == "DA05iv"
+    assert system_info.latitude == -30.960324
+    assert system_info.longitude == 73.324469
+    assert system_info.ssid == "ArednMeshNetwork"
+    assert system_info.channel == "-2"
+    assert system_info.channel_bandwidth == "10"
+    assert system_info.api_version == "1.5"
+    assert len(system_info.load_averages) == 3
+    assert system_info.up_time == "0 days, 2:39:38"
+    assert system_info.active_tunnel_count == 0
+    assert not system_info.tunnel_installed
+
+
+def test_api_version_1_6(data_folder):
+    """Test parsing API version 1.6"""
+
+    with open(data_folder / "sysinfo-1.6-services.json", "r") as f:
+        json_data = json.load(f)
+    system_info = crawler._load_node_data(json_data)
+
+    # I could just construct a second object but I'm not checking everything
+    assert system_info.node_name == "N0CALL-NSM2-3-East-Hills"
+    assert system_info.description == "Elevation 1850' Pointing WSW"
+    assert len(system_info.interfaces) == 11
+    assert system_info.interfaces["eth0"].ip_address is None
+    assert system_info.model == "NanoStation M2 "
+    assert system_info.grid_square == "EH02ht"
+    assert system_info.latitude == 54.894873
+    assert system_info.longitude == 77.502536
+    assert system_info.ssid == "ArednMeshNetwork"
+    assert system_info.channel == "-2"
+    assert system_info.channel_bandwidth == "10"
+    assert system_info.api_version == "1.6"
+    assert len(system_info.load_averages) == 3
+    assert system_info.up_time == "255 days, 3:00:03"
+    assert system_info.active_tunnel_count == 0
+    assert not system_info.tunnel_installed
+    assert len(system_info.services) == 1
+    assert system_info.wifi_ip_address == "10.159.123.176"
+    assert system_info.band == "2GHz"
+
+
+def test_api_version_1_7(data_folder):
+    """Test parsing API version 1.7"""
+
+    with open(data_folder / "sysinfo-1.7-link_info.json", "r") as f:
+        json_data = json.load(f)
+    system_info = crawler._load_node_data(json_data)
+
+    # I could just construct a second object but I'm not checking everything
+    assert system_info.node_name == "N0CALL-VC-RF-5G"
+    assert len(system_info.interfaces) == 6
+    assert system_info.interfaces["eth0"].ip_address is None
+    assert system_info.model == "MikroTik RouterBOARD LHG 5HPnD-XL"
+    assert system_info.grid_square == "AH58ku"
+    assert system_info.latitude == -18.627378
+    assert system_info.longitude == 56.804502
+    assert system_info.ssid == "ArednMeshNetwork"
+    assert system_info.channel == "177"
+    assert system_info.channel_bandwidth == "20"
+    assert system_info.api_version == "1.7"
+    assert len(system_info.load_averages) == 3
+    assert system_info.up_time == "3 days, 19:44:05"
+    assert system_info.active_tunnel_count == 0
+    assert not system_info.tunnel_installed
+    assert system_info.wifi_ip_address == "10.106.204.11"
+    assert system_info.band == "5GHz"
+
+
 @pytest.mark.asyncio
 async def test_get_nodes(data_folder):
     """Verify some basic information about `crawler.get_nodes()`"""
     records = olsr_records(data_folder / "olsr-dump.txt")
-    nodes = [node async for node in crawler.get_nodes(records)]
+    nodes = [node async for node in crawler._get_node_addresses(records)]
 
     assert len(nodes) == 23
     assert nodes[0] == "10.122.183.8"
@@ -26,7 +147,7 @@ async def test_get_nodes(data_folder):
 async def test_get_nodes_unique(data_folder):
     """Verify that the node parser is only returning unique nodes."""
     records = olsr_records(data_folder / "olsr-dump.txt")
-    nodes = [node async for node in crawler.get_nodes(records)]
+    nodes = [node async for node in crawler._get_node_addresses(records)]
 
     assert len(set(nodes)) == len(nodes)
 
@@ -36,7 +157,10 @@ async def test_get_nodes_ignore_hosts(data_folder):
     """Verify that ignored hosts are excluded from the node list."""
     records = olsr_records(data_folder / "olsr-dump.txt")
     nodes = {
-        node async for node in crawler.get_nodes(records, ignore_hosts={"10.122.183.8"})
+        node
+        async for node in crawler._get_node_addresses(
+            records, addresses_to_ignore={"10.122.183.8"}
+        )
     }
 
     assert "10.122.183.8" not in nodes
@@ -46,6 +170,7 @@ async def test_get_nodes_ignore_hosts(data_folder):
 @pytest.mark.asyncio
 async def test_get_link_info(data_folder):
     records = olsr_records(data_folder / "olsr-dump.txt")
-    links = [link async for link in crawler.get_links(records)]
+    links = [link async for link in crawler._get_node_links(records)]
 
     assert len(links) == 96
+    assert links[0] == crawler.LinkInfo("10.22.15.88", "10.98.33.29", 2.986)
