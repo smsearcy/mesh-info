d02b38f1fec37773a424e7a70f7d398518eb69c8
Scott Searcy <smsearcy14@outlook.com>
2021-08-20 15:15:14 -0700

    Use Vanilla JS instead of Vue
    Decided not to commit to a Javascript framework yet

--- pymeshmap/static/js/meshmap.js
+++ pymeshmap/static/js/meshmap.js
@@ -1,20 +1,25 @@
-/*
-  Disclaimer: I'm not a frontend developer and I'm learning Vue.js
-  (And I want the simplicity of no Javascript build process.)
-  -Scott
-*/
+document.addEventListener('DOMContentLoaded', () => {
 
-const app = Vue.createApp({
-  data() {
-    return {
-      menuExpanded: false
-    }
-  },
-  methods: {
-    toggleMenu() {
-      this.menuExpanded = !this.menuExpanded
-    }
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
   }
-})
 
-const vm = app.mount("#app")
+});
