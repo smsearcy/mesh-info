2eca265b4cdce4f255cf897ca8b8810f1c4d9547
Scott Searcy <smsearcy14@outlook.com>
2022-03-15 13:53:18 -0700

    Bump current AREDN versions

--- pymeshmap/config.py
+++ pymeshmap/config.py
@@ -35,8 +35,8 @@ class AppConfig:
 
     @environ.config
     class Aredn:
-        current_firmware: str = environ.var(default="3.21.4.0")
-        current_api: str = environ.var(default="1.8")
+        current_firmware: str = environ.var(default="3.22.1.0")
+        current_api: str = environ.var(default="1.9")
 
     @environ.config
     class Collector:
