4c7343741aa77aac4766ed05e9c376fb2898bf17
Scott Searcy <smsearcy14@outlook.com>
2022-09-21 13:16:21 -0700

    Update current AREDN version

--- meshinfo/config.py
+++ meshinfo/config.py
@@ -49,7 +49,7 @@ def default_workers():
 class AppConfig:
     @environ.config
     class Aredn:
-        current_firmware: str = environ.var(default="3.22.6.0")
+        current_firmware: str = environ.var(default="3.22.8.0")
         current_api: str = environ.var(default="1.11")
 
     @environ.config
