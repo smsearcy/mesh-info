5ebda4c488a1cdc0d660def591d658a967057e66
Scott Searcy <smsearcy14@outlook.com>
2024-07-08 13:17:02 -0700

    Update a couple pages to use 2.0 `select` statements

--- meshinfo/views/iperf.py
+++ meshinfo/views/iperf.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from pyramid.request import Request
 from pyramid.view import view_config
+from sqlalchemy import sql
 from sqlalchemy.orm import Session
 
 from ..models import Node
@@ -12,5 +13,7 @@ from ..types import NodeStatus
 def overview(request: Request):
     dbsession: Session = request.dbsession
 
-    nodes = dbsession.query(Node).filter(Node.status != NodeStatus.INACTIVE).all()
+    nodes = dbsession.execute(
+        sql.select(Node).where(Node.status != NodeStatus.INACTIVE)
+    ).scalars()
     return {"nodes": nodes}
